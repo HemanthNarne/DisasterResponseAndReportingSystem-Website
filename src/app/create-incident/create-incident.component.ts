575ea87fd96486578ec97c232def679d85c13215
HemanthNarne <s530945@nwmissouri.edu>
2018-06-28 19:00:01 -0500

    added alert msg

--- src/app/create-incident/create-incident.component.ts
+++ src/app/create-incident/create-incident.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-incident',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateIncidentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onCreate(createInc){
+    alert("Module Created Successfully");
+    this.router.navigate(['/dashboard']);
+    
   }
 
 }
