3de8eab46cf0179e14cf5f07d79ada0e8eb81fe3
Kalburgi Srinivas <S530468@nwmissouri.edu>
2018-07-09 04:09:18 -0500

    selecting applicant form table is wokring

--- src/app/review-application/review-application.component.ts
+++ src/app/review-application/review-application.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { applicant } from '../common/mock-applicants';
 import {MatPaginator, MatTableDataSource, MatSort} from '@angular/material';
 import { Applicant } from '../common/applicant';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-review-application',
@@ -15,9 +16,11 @@ export class ReviewApplicationComponent implements OnInit {
   displayedColumns = ['firstName', 'lastName', 'email', 'dob', 'county', 'skills'];
   
   dataSource = new MatTableDataSource<Applicant>(applicant);
+  selection = new SelectionModel<Applicant>(true, []);
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
+  
    
   constructor() { }
 
@@ -34,5 +37,13 @@ export class ReviewApplicationComponent implements OnInit {
     filterValue = filterValue.trim(); // Remove whitespace
     filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
     this.dataSource.filter = filterValue;
+  }
+
+  rowClicked(row: any): void {
+    console.log(row);
+    confirm("First Name: " + row.firstName +"\n"+
+            "Last Name: " + row.lastName +"\n"+
+            "County: " + row.countys
+  );
   }
 }
